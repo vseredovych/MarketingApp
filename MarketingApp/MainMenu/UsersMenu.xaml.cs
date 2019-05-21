bc7c48e612d8d9d34bfa862ac183ecd972180eac
vseredovych <victor.seredovich@gmail.com>
2019-05-21 13:31:18 +0300

    add UsersMenu

---
+++ MarketingApp/MainMenu/UsersMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DAL.Entities;
+
+namespace MarketingApp
+{
+    /// <summary>
+    /// Логика взаимодействия для People.xaml
+    /// </summary>
+    public partial class UsersMenu : UserControl
+    {
+        public ProductsCollections Products;// = new ProductRepository();
+
+        int Limit;
+        int Offset;
+        public UsersMenu()
+        {
+            InitializeComponent();
+
+
+            //for (int i = 6; i < 100; i++)
+            //{
+            //    Products.Add(new Product
+            //    {
+            //        Id = i,
+            //        Name = "Name" + i,
+            //        CreatedAt = Convert.ToDateTime("2019-10-10"),
+            //        MerchantId = i,
+            //        Status = "Present"
+            //    }
+            //    );
+            //}
+        }
+    }
+}
